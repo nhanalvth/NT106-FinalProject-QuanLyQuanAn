133eaf8d8c0405eac10e1108edf1b621381db60b
nhanalvth <115458514+nhanalvth@users.noreply.github.com>
2025-04-01 13:00:19 +0700

    Tạo banner trên Dashboard

--- FinalProject/Dashboard.xaml.cs
+++ FinalProject/Dashboard.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FinalProject
 {
@@ -20,9 +22,48 @@ namespace FinalProject
     /// </summary>
     public partial class Dashboard : Page
     {
+        private ObservableCollection<string> currentImage = new ObservableCollection<string>(); // Danh sách chứa ảnh hiện tại
+        private List<string> imagePaths = new List<string> // Danh sách ảnh
+        {
+            "/Images/banner1.jpg",
+            "/Images/banner2.jpg",
+            "/Images/banner3.jpg",
+            "/Images/banner4.jpg"
+        };
+
+        private int currentIndex = 0;
+        private DispatcherTimer timer;
         public Dashboard()
         {
             InitializeComponent();
+            InitializeComponent();
+            currentImage.Add(imagePaths[currentIndex]); // Thêm ảnh đầu tiên vào danh sách hiển thị
+            BannerSlider.ItemsSource = currentImage;   // Gán Binding
+
+            // Tạo timer để tự động chuyển ảnh
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += (s, e) => NextImage(null, null);
+            timer.Start();
         }
+
+        private void NextImage(object sender, RoutedEventArgs e)
+        {
+            currentIndex = (currentIndex + 1) % imagePaths.Count;
+            UpdateImage();
+        }
+
+        private void PrevImage(object sender, RoutedEventArgs e)
+        {
+            currentIndex = (currentIndex - 1 + imagePaths.Count) % imagePaths.Count;
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            currentImage[0] = imagePaths[currentIndex]; // Cập nhật ảnh hiện tại
+        }
+
+
     }
 }
