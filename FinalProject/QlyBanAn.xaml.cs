a7f24d3233d891680b001815ada92e42e0606e35
nhanalvth <115458514+nhanalvth@users.noreply.github.com>
2025-05-10 09:36:55 +0700

    tạo UI Qly Bàn Ăn và Qly Nhân viên

--- FinalProject/QlyBanAn.xaml.cs
+++ FinalProject/QlyBanAn.xaml.cs
@@ -26,5 +26,25 @@ namespace FinalProject
         {
             InitializeComponent();          
         }
+        private void Ban_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            string banSo = btn.Content.ToString();
+            string trangThai = btn.Tag.ToString();
+
+            MessageBox.Show($"{banSo} - Trạng thái: {HienThiTrangThai(trangThai)}", "Thông tin bàn");
+        }
+
+        private string HienThiTrangThai(string tag)
+        {
+            return tag switch
+            {
+                "Trong" => "Bàn trống",
+                "DangPhucVu" => "Đang phục vụ",
+                "DaThanhToan" => "Đã thanh toán",
+                "ChuaDonDep" => "Chưa dọn dẹp",
+                _ => "Không xác định"
+            };
+        }
     }
 }
--- FinalProject/QlyNhanVien.xaml.cs
+++ FinalProject/QlyNhanVien.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,62 @@ namespace FinalProject
     /// </summary>
     public partial class QlyNhanVien : Page
     {
+        public ObservableCollection<NhanVien> DanhSachNhanVien = new ObservableCollection<NhanVien>();
         public QlyNhanVien()
         {
             InitializeComponent();
+            dgNhanVien.ItemsSource = DanhSachNhanVien;
+        }
+        private void BtnThem_Click(object sender, RoutedEventArgs e)
+        {
+            var nv = new NhanVien
+            {
+                MaNV = txtMaNV.Text,
+                TenNV = txtTenNV.Text,
+                VaiTro = ((ComboBoxItem)cbVaiTro.SelectedItem)?.Content.ToString(),
+                IsQuanLy = chkQuanLy.IsChecked ?? false,
+                CaLamViec = txtCaLam.Text
+            };
+            DanhSachNhanVien.Add(nv);
+            ClearForm();
+        }
+
+        private void BtnSua_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgNhanVien.SelectedItem is NhanVien nv)
+            {
+                nv.TenNV = txtTenNV.Text;
+                nv.VaiTro = ((ComboBoxItem)cbVaiTro.SelectedItem)?.Content.ToString();
+                nv.IsQuanLy = chkQuanLy.IsChecked ?? false;
+                nv.CaLamViec = txtCaLam.Text;
+                dgNhanVien.Items.Refresh();
+            }
+        }
+
+        private void BtnXoa_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgNhanVien.SelectedItem is NhanVien nv)
+            {
+                DanhSachNhanVien.Remove(nv);
+            }
+        }
+
+        private void ClearForm()
+        {
+            txtMaNV.Clear();
+            txtTenNV.Clear();
+            cbVaiTro.SelectedIndex = -1;
+            chkQuanLy.IsChecked = false;
+            txtCaLam.Clear();
         }
     }
+    public class NhanVien
+    {
+        public string MaNV { get; set; }
+        public string TenNV { get; set; }
+        public string VaiTro { get; set; }
+        public bool IsQuanLy { get; set; }
+        public string CaLamViec { get; set; }
+    }
 }
+
