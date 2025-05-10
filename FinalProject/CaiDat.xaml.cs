cc016665c0c10cbf43737fede0be846f09d3b041
nhanalvth <115458514+nhanalvth@users.noreply.github.com>
2025-05-10 10:11:18 +0700

    tạo ui Cài đặt

--- FinalProject/CaiDat.xaml.cs
+++ FinalProject/CaiDat.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,30 @@ namespace FinalProject
         {
             InitializeComponent();
         }
+        private void BtnChonLogo_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files (*.png;*.jpg)|*.png;*.jpg";
+            if (dialog.ShowDialog() == true)
+            {
+                MessageBox.Show($"Logo đã chọn: {dialog.FileName}", "Thông báo");
+            }
+        }
+
+        private void BtnLuu_Click(object sender, RoutedEventArgs e)
+        {
+            string tenQuan = txtTenQuan.Text;
+            string diaChi = txtDiaChi.Text;
+            string sdt = txtSDT.Text;
+            string vaiTro = ((ComboBoxItem)cbPhanQuyen.SelectedItem)?.Content.ToString();
+            string vat = txtVAT.Text;
+            bool thongBao = chkThongBao.IsChecked ?? false;
+            string ngonNgu = ((ComboBoxItem)cbNgonNgu.SelectedItem)?.Content.ToString();
+            string cheDo = ((ComboBoxItem)cbCheDo.SelectedItem)?.Content.ToString();
+
+            MessageBox.Show($"Đã lưu cài đặt:\n- Tên quán: {tenQuan}\n- Vai trò: {vaiTro}\n- VAT: {vat}%\n- Ngôn ngữ: {ngonNgu}\n- Chế độ: {cheDo}\n- Thông báo: {(thongBao ? "Bật" : "Tắt")}",
+                            "Lưu thành công", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }
+
