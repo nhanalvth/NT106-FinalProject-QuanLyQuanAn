7e0fb26eb090d18d880904721ef3ac94ae40cfc5
thanhtrum <23521438@gm.uit.edu.vn>
2025-07-11 21:13:22 +0700

    them che do loc va cham cong
    them che do loc va cham cong

--- FinalProject/QlyNhanVien.xaml.cs
+++ FinalProject/QlyNhanVien.xaml.cs
@@ -1,37 +1,63 @@
-﻿using FinalProject.Models;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Npgsql;
 
 namespace FinalProject
 {
-    /// <summary>
-    /// Interaction logic for QlyNhanVien.xaml
-    /// </summary>
     public partial class QlyNhanVien : Page
     {
-        // Chuỗi kết nối đến cơ sở dữ liệu PostgreSQL
         private readonly string connectionString = "Host=ep-super-frost-a1wzegym-pooler.ap-southeast-1.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_NZgous1jTzB9;SSL Mode=Require;Trust Server Certificate=true";
 
-        public ObservableCollection<NhanVien> DanhSachNhanVien = new ObservableCollection<NhanVien>();
+        // Danh sách nhân viên
+        public ObservableCollection<NhanVien> DanhSachNhanVien { get; set; } = new ObservableCollection<NhanVien>();
+
+        // Danh sách chấm công
+        public ObservableCollection<ChamCong> DanhSachChamCong { get; set; } = new ObservableCollection<ChamCong>();
+
         public QlyNhanVien()
         {
             InitializeComponent();
-            txtUsername.Text = UserSession.UserName;
+            txtUsername.Text = "Admin";
             dgNhanVien.ItemsSource = DanhSachNhanVien;
+            dgChamCong.ItemsSource = DanhSachChamCong;
+            dpNgayChamCong.SelectedDate = DateTime.Today;
+
+            LoadNhanVienFromDatabase();
         }
+
+        // Load dữ liệu nhân viên từ database
+        private void LoadNhanVienFromDatabase()
+        {
+            try
+            {
+                using (var conn = new NpgsqlConnection(connectionString))
+                {
+                    conn.Open();
+                    var cmd = new NpgsqlCommand("SELECT * FROM nhanvien", conn);
+                    var reader = cmd.ExecuteReader();
+
+                    DanhSachNhanVien.Clear();
+                    while (reader.Read())
+                    {
+                        DanhSachNhanVien.Add(new NhanVien
+                        {
+                            MaNV = reader["manv"].ToString(),
+                            TenNV = reader["tennv"].ToString(),
+                            VaiTro = reader["vaitro"].ToString(),
+                            IsQuanLy = Convert.ToBoolean(reader["isquanly"]),
+                            CaLamViec = reader["calamviec"].ToString()
+                        });
+                    }
+                }
+            }
+            catch { /* Xử lý lỗi nếu cần */ }
+        }
+
+        // Thêm nhân viên mới
         private void BtnThem_Click(object sender, RoutedEventArgs e)
         {
             var nv = new NhanVien
@@ -46,6 +72,7 @@ namespace FinalProject
             ClearForm();
         }
 
+        // Sửa thông tin nhân viên
         private void BtnSua_Click(object sender, RoutedEventArgs e)
         {
             if (dgNhanVien.SelectedItem is NhanVien nv)
@@ -58,6 +85,7 @@ namespace FinalProject
             }
         }
 
+        // Xóa nhân viên
         private void BtnXoa_Click(object sender, RoutedEventArgs e)
         {
             if (dgNhanVien.SelectedItem is NhanVien nv)
@@ -66,6 +94,49 @@ namespace FinalProject
             }
         }
 
+        // Chấm công
+        private void BtnCheckInOut_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgNhanVien.SelectedItem is NhanVien nv && dpNgayChamCong.SelectedDate.HasValue)
+            {
+                var ngay = dpNgayChamCong.SelectedDate.Value;
+                var gio = DateTime.Now.ToString("HH:mm");
+
+                var chamCong = DanhSachChamCong.FirstOrDefault(cc =>
+                    cc.MaNV == nv.MaNV && cc.Ngay.Date == ngay.Date);
+
+                if (chamCong == null)
+                {
+                    // Check-in lần đầu
+                    DanhSachChamCong.Add(new ChamCong
+                    {
+                        MaNV = nv.MaNV,
+                        TenNV = nv.TenNV,
+                        Ngay = ngay,
+                        CheckIn = gio,
+                        CheckOut = ""
+                    });
+                }
+                else if (string.IsNullOrEmpty(chamCong.CheckOut))
+                {
+                    // Check-out
+                    chamCong.CheckOut = gio;
+                    dgChamCong.Items.Refresh();
+                }
+                else
+                {
+                    // Đã check-out rồi, không làm gì
+                }
+            }
+        }
+
+        // Đặt ngày về hôm nay
+        private void BtnHomNay_Click(object sender, RoutedEventArgs e)
+        {
+            dpNgayChamCong.SelectedDate = DateTime.Today;
+        }
+
+        // Clear form nhập liệu
         private void ClearForm()
         {
             txtMaNV.Clear();
@@ -74,14 +145,25 @@ namespace FinalProject
             chkQuanLy.IsChecked = false;
             txtCaLam.Clear();
         }
-    }
-    public class NhanVien
-    {
-        public string MaNV { get; set; }
-        public string TenNV { get; set; }
-        public string VaiTro { get; set; }
-        public bool IsQuanLy { get; set; }
-        public string CaLamViec { get; set; }
+
+        // Model nhân viên
+        public class NhanVien
+        {
+            public string MaNV { get; set; }
+            public string TenNV { get; set; }
+            public string VaiTro { get; set; }
+            public bool IsQuanLy { get; set; }
+            public string CaLamViec { get; set; }
+        }
+
+        // Model chấm công
+        public class ChamCong
+        {
+            public string MaNV { get; set; }
+            public string TenNV { get; set; }
+            public DateTime Ngay { get; set; }
+            public string CheckIn { get; set; }
+            public string CheckOut { get; set; }
+        }
     }
 }
-
